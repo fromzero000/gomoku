ffd7f7f042131a62bb03aefc3c600fffe46e326c
MUJIN KANG <fromzero0427@gmail.com>
2026-03-08 11:48:58 +0900

    소켓 통신 테스트 1:1 Echo 구현

--- client/client.cpp
+++ client/client.cpp
@@ -1,19 +1,75 @@
 ﻿#include <iostream>
 #include <vector>
-#include <string>
+#include <sstream>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
-constexpr int BUF_SIZE = 10;
+constexpr int BUF_SIZE = 1024;
+
+void ErrorHandling(const std::string& message, SOCKET hServSock = INVALID_SOCKET);
 
 int main(int argc, char *argv[])
 {
     WSAData wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        return 1;
-    }
+        ErrorHandling("WSAStartup() error");
+
+    SOCKET hServSock;
+    sockaddr_in servAddr{};
+
+    std::string message;
+    int strLen;
     
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServSock == INVALID_SOCKET)
+        ErrorHandling("socket() error");
+
+    servAddr.sin_family = AF_INET;
+    int inetPtonResult = inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
+    //visual studio에서 inetPton 사용시 유니코드 관련 에러 발생
+    if (inetPtonResult != 1)
+    {
+        if (inetPtonResult == 0)
+            ErrorHandling("Invalid IP Address", hServSock);
+        else
+        {
+            int errCode = WSAGetLastError();
+            ErrorHandling("inet_pton() error, error Code : "+std::to_string(errCode), hServSock);
+        }
+    }
+    servAddr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(hServSock, (sockaddr*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+        ErrorHandling("connect() error", hServSock);
+    else
+        std::cout << "Connected...\n";
+
+    while (1)
+    {
+        std::cout << "Input Message(Q to quit) : ";
+        std::getline(std::cin, message);
+        if (message == "Q" || message == "q")
+            break;
+
+        message.resize(message.length());
+        send(hServSock, message.data(), message.length(), 0);
+        message.resize(BUF_SIZE);
+        strLen = recv(hServSock, message.data(), BUF_SIZE, 0);
+        if (strLen == -1)
+            ErrorHandling("recv() error", hServSock);
+        message.resize(strLen);
+        std::cout << "Echo from server : " << message << '\n';
+    }
+    closesocket(hServSock);
+    WSACleanup();
     return 0;
+}
+
+void ErrorHandling(const std::string& message, SOCKET hServSock)
+{
+    std::cerr << message << '\n';
+    if (hServSock != INVALID_SOCKET) closesocket(hServSock);
+    WSACleanup();
+    exit(1);
 }
--- server/server.cpp
+++ server/server.cpp
@@ -5,16 +5,61 @@
 #include <WS2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
-constexpr int BUF_SIZE = 10;
-#1234
+constexpr int BUF_SIZE = 1024;
+void ErrorHandling(const std::string& message, SOCKET hServSock = INVALID_SOCKET, SOCKET hClntSock = INVALID_SOCKET);
+
 int main(int argc, char* argv[])
 {
     WSAData wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        return 1;
-    }
+        ErrorHandling("WSAStartup() error");
 
-    std::cout << "hello gomoku\n";
+    SOCKET hServSock, hClntSock;
+    sockaddr_in servAddr{}, clntAddr{};
+    int clntAddrSize;
+    std::string message;
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if(hServSock == INVALID_SOCKET)
+        ErrorHandling("socket() error");
+    
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(hServSock, (sockaddr *)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+        ErrorHandling("bind() error");
+
+    if(listen(hServSock, 1) == SOCKET_ERROR)
+        ErrorHandling("listen() error");
+    clntAddrSize = sizeof(clntAddr);
+    hClntSock = accept(hServSock, (sockaddr*)&clntAddr, &clntAddrSize);
+    if (hClntSock == -1)
+        ErrorHandling("accept() error");
+    else
+        std::cout << "Connected client\n";
+    /*통신 테스트용 코드 (로직 추가 예정)*/
+    while (1)
+    {
+        message.resize(BUF_SIZE);
+        int receivedDataLen = recv(hClntSock, message.data(), BUF_SIZE, 0);
+        if (receivedDataLen <= 0)
+            break;
+        message.resize(receivedDataLen);
+        std::cout << "Received from client : " << message << '\n';
+        send(hClntSock, message.data(), receivedDataLen, 0);
+    }
+    /*통신 테스트용 코드 종료*/
+    closesocket(hClntSock);
+    closesocket(hServSock);
+    WSACleanup();
     return 0;
+}
+
+void ErrorHandling(const std::string& message, SOCKET hServSock, SOCKET hClntSock)
+{
+    std::cerr << message << '\n';
+    if (hServSock != INVALID_SOCKET) closesocket(hServSock);
+    if (hClntSock!= INVALID_SOCKET) closesocket(hClntSock);
+    WSACleanup();
+    exit(1);
 }
